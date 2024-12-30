ca6e10b37b1349416fefbe629aaf93bce438bf5a
BeatrizCGuerra <beatriz.guerra@criticalsoftware.com>
2024-12-30 22:40:31 +0000

    unit 2

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,18 @@
 #include <cstdint>
 
 int main(){
-    
+    int fahr = 100;
+    int celsius;
+
+    celsius = ((float)5 / 9.0) * (fahr - 32);
+
+    std::cout << "celsius = " << celsius << std::endl;
+
+    double weight = 10.99;
+    std::cout << "Float = " << weight << std::endl;
+    std::cout << "Integer = " << (int) weight << std::endl;
+    std::cout << "Fractional part = " << (int) (( weight - (int) weight) * 10000) << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
